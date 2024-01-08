2abe18e066a3a9e0aa778cb7e2253ec076c2dbf8
Paul Adams <ghusiyy@gmail.com>
2024-01-08 01:04:07 -0800

    add ability to add/remove task

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Task {
+  label: string;
 }
 
-export default App
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [task, setTask] = useState<string>("");
+  function addTask() {
+    setTasks((v) => [...v, { label: task }]);
+    setTask("");
+  }
+  function removeTask(task: Task) {
+    setTasks(($tasks) => $tasks.filter(($task) => $task.label !== task.label));
+  }
+  return (
+    <>
+      <h1>React Task Alert</h1>
+      <div className="card">
+        <div className="task-display">
+          <ul>
+            {tasks.map((task, i) => (
+              <li key={i}>
+                <div>{task.label}</div>
+                <div>
+                  <button onClick={() => removeTask(task)}>remove</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="task-inputs">
+          <label htmlFor="task-input">
+            <input
+              id="task-input"
+              placeholder="Enter task"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+          </label>
+          <button onClick={() => addTask()}>add</button>
+        </div>
+      </div>
+      <p className="read-the-docs"></p>
+    </>
+  );
+}
+
+export default App;
