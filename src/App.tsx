65bf3788bff025674ba8b5b960edcaee289a14fb
Paul Adams <ghusiyy@gmail.com>
2024-01-08 01:23:14 -0800

    add ability to mark as complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 interface Task {
+  id: string;
   label: string;
+  completed?: boolean;
+  reminder?: {
+    date: Date;
+  };
 }
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [task, setTask] = useState<string>("");
   function addTask() {
-    setTasks((v) => [...v, { label: task }]);
+    setTasks((v) => [...v, { id: nanoid(), label: task }]);
     setTask("");
   }
   function removeTask(task: Task) {
-    setTasks(($tasks) => $tasks.filter(($task) => $task.label !== task.label));
+    setTasks(($tasks) => $tasks.filter(($task) => $task.id !== task.id));
+  }
+  function markAsComplete(task: Task) {
+    setTasks(($tasks) =>
+      $tasks.map(($task) =>
+        $task.id === task.id ? { ...$task, completed: !$task.completed } : $task
+      )
+    );
   }
   return (
     <>
       <h1>React Task Alert</h1>
       <div className="card">
+        <div className="task-operations">
+          <button>modify all</button>
+          <button>complete all</button>
+          <button>delete all</button>
+        </div>
         <div className="task-display">
           <ul>
             {tasks.map((task, i) => (
               <li key={i}>
-                <div>{task.label}</div>
                 <div>
+                  <input type="checkbox" />
+                  <div
+                    className={`task-label ${
+                      task.completed ? "completed" : ""
+                    }`}
+                  >
+                    {task.label}
+                  </div>
+                </div>
+                <div>
+                  <button onClick={() => markAsComplete(task)}>complete</button>
                   <button onClick={() => removeTask(task)}>remove</button>
                 </div>
               </li>
